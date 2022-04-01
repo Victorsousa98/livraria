29c6aea201b5f50a9fba6da6e5ecb98b579578cd
Victorsousa98 <victorsousa708@gmail.com>
2022-04-01 02:09:47 -0300

    git ignore, rotas

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const http = require('http');
 const port = 3000;
 
 
+
+
+//rotas
+    const rotas = {
+        '/': 'Livraria',
+        '/livros': 'Livros',
+        '/autores': 'Autores',
+        '/editoras': 'Editoras'
+    }
+
+
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(rotas[req.url]);
 });
-
-
-
-
-
-
-
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
