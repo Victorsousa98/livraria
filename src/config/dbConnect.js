d20e4bbf3ac15d122a06aeee35e396e5970b1dbb
Victorsousa98 <85964942+Victorsousa98@users.noreply.github.com>
2022-04-05 02:07:47 -0300

    A

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-    "mongodb+srv://Victorsousa98:23071979jid@cluster0.0unpx.mongodb.net/victorsousa"
+    "mongodb+srv://Victorsousa98:password@cluster0.0unpx.mongodb.net/victorsousa"
 );
 
 let db = mongoose.connection;//criamos uma variável que recebe a conexão com o banco de dados
